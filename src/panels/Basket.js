f6b899fcc724216afa291e6f78182a4422993c8c
Dmitry <dimka11lp@gmail.com>
2020-09-10 10:57:36 +0600

    init

---
+++ src/panels/Basket.js
@@ -0,0 +1,201 @@
+import React, { useMemo, useState, useEffect  } from 'react';
+import { withRouter, Link } from 'react-router-dom';
+import accounting from 'accounting';
+
+import Checkbox from './Checkbox';
+
+import edit from '../img/edit.svg';
+import './place.css';
+
+function check_time(time) {
+  var patt = new RegExp("^([0-1]?[0-9]|2[0-3])[:|.][0-5][0-9]$");
+  var res = patt.test(time);
+  return res;
+}
+
+
+
+const Basket = ({ match: { params: { areaId, itemId }}, foodAreas, order }) => {
+  const [ faster, setFaster ] = useState(true);
+  const [ time, setTime ] = useState('');
+  const [ selfService, setSelfService ] = useState(false);
+  const area = foodAreas.filter(area => area.id === areaId)[0];
+  const item = area.items.filter(item => item.id === itemId)[0];
+
+  function save_to_ls() {
+    var order_info = new Object();
+    order_info.cafe = item.id;
+    order_info.time = time;
+    order_info.faster = faster;
+    order_info.place_choice = selfService;
+
+    var orders_info = new Map (JSON.parse(localStorage.getItem('orders_info') || 'null'))
+    orders_info.set(order_info.cafe, order_info)
+    localStorage.setItem('orders_info',  JSON.stringify([...orders_info]));
+  }
+
+  const [ price, products ] = useMemo(() => {
+    const foodIds = new Set((item.foods || []).map(item => item.id));
+
+    const products = Object.values(order)
+      .filter((value) => {
+        const { item: { id }} = value;
+
+        return foodIds.has(id);
+      });
+
+    const result = products.reduce((result, value) => {
+        const { count, item } = value;
+
+        return result + parseInt(item.price) * parseInt(count);
+      }, 0);
+
+    return [ accounting.formatNumber(result, 0, ' '), products ];
+  }, [ order, item ]);
+
+
+  useEffect(() => {
+    var orders_info = new Map (JSON.parse(localStorage.getItem('orders_info') || 'null'))
+    var order_info = orders_info.get(item.id);
+    if (order_info != undefined) {
+      setTime(order_info.time);
+      setFaster(order_info.faster);
+      setSelfService(order_info.place_choice);
+    }
+}, []); // passing an empty array as second argument triggers the callback in useEffect only after the initial render thus replicating `componentDidMount` lifecycle behaviour
+
+  return (
+    <div className="Place">
+      <header className="Place__header">
+        <aside className="Place__trz">
+          <h1 className="Place__head">
+            <Link to="/" className="Place__logo">
+              {area.name}
+            </Link>
+          </h1>
+          <Link to="/edit" className="Place__change-tz">
+            <img
+              alt="change-profile"
+              src={edit}
+            />
+          </Link>
+        </aside>
+      </header>
+      <aside className="Place__restoraunt">
+        <img
+          className="Place__restoraunt-logo"
+          alt="Fastfood logo"
+          src={item.image}
+        />
+        <h2
+          className="Place__restoraunt-name"
+        >
+          {item.name}
+        </h2>
+        <p className="Place__restoraunt-type">
+          {item.description}
+        </p>
+      </aside>
+      <div className="Place__products-wrapper">
+        <ul className="Place__products">
+          {products.map(({ item, count }) => (
+            <li
+              className="Place__product"
+              key={item.id}
+            >
+              <img
+                className="Place__product-logo"
+                alt="Ordered product logo"
+                src={item.image}
+              />
+              <h3
+                className="Place__product-name"
+              >
+                {item.name}
+              </h3>
+              <p
+                className="Place__product-price"
+              >
+                Цена: {item.price}
+              </p>
+              <p
+                className="Place__product-count"
+              >
+                x{count}
+              </p>
+            </li>
+          ))}
+        </ul>
+        <Link
+          className="Place__change-product"
+          to={`/place/${areaId}/${itemId}`}
+        >
+          Изменить
+        </Link>
+      </div>
+      <div className="Place__choice">
+        <h3>Время:</h3>
+        <div className="Place__choice-item">
+          <span>Как можно быстрее</span>
+          <Checkbox 
+            checked={faster} 
+            onToggle={() => {
+              if (faster) {
+                setFaster(false);
+              } else {
+                setTime('');
+                setFaster(true);
+              }
+            }}
+          />
+        </div>
+        <div className="Place__choice-item">
+          <span>Назначить</span>
+          <input
+            value={time}
+            onFocus={() => {
+              setFaster(false);
+            }}
+            onChange={event => {
+              setFaster(false);
+              setTime(event.target.value);
+            }}
+            onBlur={() => {
+              if (time) {
+                setFaster(false);
+              }
+
+              if (time.length > 0 && time.length != 5) {
+                alert('время введено неправильно!')
+                setTime('');
+                setFaster(true);
+              }
+
+              if (!check_time(time)) {
+                alert('время введено неправильно!')
+                setTime('');
+                setFaster(true);
+              }
+
+            }}
+          />
+        </div>
+        <div className="Place__choice-item">
+          <h3>С собой</h3>
+          <Checkbox checked={selfService} onToggle={() => setSelfService(!selfService)} />
+        </div>
+        <div className="Place__choice-item">
+          <h3>На месте</h3>
+          <Checkbox checked={!selfService} onToggle={() => setSelfService(!setSelfService)} />
+        </div>
+      </div>
+      <footer className="Place__footer">
+        <Link to={`/order/${area.id}/${item.id}`} className="Place__order" onClick={save_to_ls}>
+          Оплатить {price}
+        </Link>
+      </footer>
+    </div>
+  );
+};
+
+export default withRouter(Basket);
